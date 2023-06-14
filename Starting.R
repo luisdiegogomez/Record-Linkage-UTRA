f5d0fe7375a7e1308cb320c8b8c1ef3d410e69e8
efiaawuah <efiaawuah@outlook.com>
2023-06-14 16:26:36 -0400

    save

--- Starting.R
+++ Starting.R
@@ -1,2 +1,27 @@
 
+main <- function(file_A, file_B){ 
+  A <- read.csv(file_A)
+  B <- read.csv(file_B)
+  
+  data_A = A[, intersect(names(A), names(B))]
+  data_B = B[, intersect(names(A), names(B))]
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+
+}
+
+fill_comparison_array <- function(record_a, record_b){ 
+  
+  }
+
+
+
 
