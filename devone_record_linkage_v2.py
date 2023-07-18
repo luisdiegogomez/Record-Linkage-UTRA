a041910f2d447c4f3cb880b425bbb081e1a98c92
luisdiegogomez <luisdiego_gomez@brown.edu>
2023-07-18 13:31:56 -0400

    Brick wall

--- devone_record_linkage_v2.py
+++ devone_record_linkage_v2.py
@@ -13,12 +13,6 @@ A_temp = pd.read_csv(r"C:\Users\luisd\OneDrive\Documents\R\Record-Linkage-UTRA\g
 B_temp = pd.read_csv(r"C:\Users\luisd\OneDrive\Documents\R\Record-Linkage-UTRA\generated_csv2.csv")
 
 ## Global Variables:
-
-# N_a = len(A.index)
-# N_b = len(B.index)
-
-A = 0
-B = 0
 N_a = 0
 N_b = 0
 if len(A_temp.index) >= len(B_temp.index):
