88cc1ed048730f09ce6b20d4f1d3980f45a996ed
luisdiegogomez <luisdiego_gomez@brown.edu>
2023-08-01 12:00:11 -0400

    Optomized L Instances Structure Final

--- devone_record_linkage_v4.py
+++ devone_record_linkage_v4.py
@@ -1,10 +1,16 @@
-### DEVONE IMPLEMENTATION V2: L instead of 2 agreement levels (determined by jaro winkler)
+### DEVONE IMPLEMENTATION V4: Structure to contain number of instances of l disagreement occurring
 
 import numpy as np
 
 import pandas as pd
 
 import math as math
+
+# pip install jaro-winkler
+import jaro
+
+import time
+start_time = time.time()
 
 ## Initilizating Datasets From CSV Files:
 
@@ -35,79 +41,52 @@ K = len(X_a.columns)
 L_k = np.arange(0, 1.1 ,0.1)
 L_k_n = len(L_k) # Levels of disagreement (100 for 2 decimal place values of Jaro-Winkler Distance)
 
-comparison_arrays = np.full((K, (N_a*N_b)), fill_value = 0, dtype= float) 
+comparison_arrays = np.full((K, (N_a*N_b)), fill_value = 0, dtype= float)
+base_l_instaces = np.full((K, L_k_n, 2), fill_value=0, dtype=int)
 
 #Returns jaro_winkler_distance of two strings
 def jaro_winkler_distance(s1, s2):
-    # Jaro distance
-    len_s1, len_s2 = len(s1), len(s2)
-    max_dist = max(len_s1, len_s2) // 2 - 1
-    matches = 0
-    transpositions = 0
+    jaro_winkler = round(jaro.jaro_winkler_metric(s1,s2),1)
 
-    # Find matching characters
-    for i in range(len_s1):
-        start = max(0, i - max_dist)
-        end = min(i + max_dist + 1, len_s2)
-        for j in range(start, end):
-            if s1[i] == s2[j]:
-                matches += 1
-                if i != j:
-                    transpositions += 1
-                break
-
-    if matches == 0:
-        return 0.0
-
-    jaro = (
-        (matches / len_s1)
-        + (matches / len_s2)
-        + ((matches - transpositions / 2) / matches)
-    ) / 3
-
-    # Winkler modification
-    prefix_len = 0
-    for i in range(min(len_s1, len_s2)):
-        if s1[i] == s2[i]:
-            prefix_len += 1
-        else:
-            break
-
-    jaro_winkler = round(jaro + (0.1 * prefix_len * (1 - jaro)), 1)
-    if jaro_winkler > 1: jaro_winkler = 1
+    if jaro_winkler > 1: 
+        jaro_winkler = 1.0
     
     return jaro_winkler
 
 ## Filling in Comparison Vectors (Gamma Vectors):
-def fill_comparison_arrays() :
+def fill_comparison_arrays():
     # Filling comparison vectors:
     for a in range(N_a):
         for b in range(N_b):
             for k in range(K):
-                comparison_arrays[k, ((N_b*a) + b)] = jaro_winkler_distance(str(X_a.iat[a,k]), str(X_b.iat[b,k]))
+                distance = jaro_winkler_distance(str(X_a.iat[a,k]), str(X_b.iat[b,k]))
+                comparison_arrays[k, ((N_b*a) + b)] = distance
+                base_l_instaces[k,int((10*distance)),0] += 1
 
 #Gibbs sampler 
 def theta_and_c_sampler(T:int) -> np.ndarray:
     #Establishing initial parameters for the Dirchlet Distributions from which we're sampling:
     M_alpha_priors = np.full(L_k_n, 1, dtype=int)
     U_alpha_priors = np.full(L_k_n, 1, dtype=int)
+    
     ## Gibbs Sampler for Theta Values:
     theta_values = np.full((T, K, 2, L_k_n), 0.00, dtype=float) # Array with K rows (for number of iterations)
                                          # F columns (one for each comparison variable), and 
                                          # two theta values vectors in each cell (Theta_M and Theta_U 
                                          # vectors of length L_f)
     C = np.full((N_a*N_b), 0)
-    
+    temp_l_instances = np.full((K, L_k_n, 2), fill_value=0, dtype=int)
+    temp_l_instances[:,:,0] = base_l_instaces[:,:,0]
+    temp_l_instances[:,:,1] = base_l_instaces[:,:,1]
+
     #fills dirichlet parameters for theta_M  or theta_U depending on if theta_M == True or False
-    def alpha_fill(k: int, theta_type: bool) -> np.ndarray: 
+    def alpha_fill(k: int, theta_type: bool, l_instances: np.ndarray) -> np.ndarray: 
         a_lst = []
-        for l in range(L_k_n):
-            a_kl = 0 
-            for a in range(N_a): 
-                for b in range(N_b): 
-                    a_kl += (comparison_arrays[k, N_b* a + b] == round(L_k[l],1))*(C[N_b* a + b] == theta_type)
-            if theta_type: a_lst.append(a_kl + M_alpha_priors[l])
-            else: a_lst.append(a_kl + U_alpha_priors[l])
+        for l in range(L_k_n): 
+            if theta_type: 
+                a_lst.append((l_instances[k,l,1] + M_alpha_priors[l]))
+            else: 
+                a_lst.append((l_instances[k,l,0] + U_alpha_priors[l]))
         alpha_params = np.array(a_lst)
         return alpha_params
     
@@ -127,18 +106,19 @@ def theta_and_c_sampler(T:int) -> np.ndarray:
         return lr
 
     for t in range(T):
-        #Step 1: sampling thetas 
+        #Step 1: sampling thetas
         for k in range(K):
             ## Sampling for Theta_M Values:
-            M_alpha_vec = alpha_fill(k, True)
-            theta_values[t,k, 0] = np.random.dirichlet(M_alpha_vec)
+            M_alpha_vec = alpha_fill(k, True, temp_l_instances)
+            theta_values[t,k,0] = np.random.dirichlet(M_alpha_vec)
             ## Sampling for Theta_U Values:
-            U_alpha_vec = alpha_fill(k, False)
-            theta_values[t,k, 1] = np.random.dirichlet(U_alpha_vec)
+            U_alpha_vec = alpha_fill(k, False, temp_l_instances)
+            theta_values[t,k,1] = np.random.dirichlet(U_alpha_vec)
 
         #Step 2: sampling C
-        #C[t+1]: empty 
         C = np.full((N_a*N_b), 0)
+        temp_l_instances[:,:,0] = base_l_instaces[:,:,0]
+        temp_l_instances[:,:,1] = base_l_instaces[:,:,1]
 
         row_order_list = ([a for a in range(N_a)])
         np.random.shuffle(row_order_list)
@@ -167,17 +147,27 @@ def theta_and_c_sampler(T:int) -> np.ndarray:
             
             #last index in index list == no_link. if it selected a valid index, we want 
             if(new_link_index != len(b_unlinked)):   
-                # print(a, b_unlinked[new_link_index], link_probs[new_link_index])
-                C[N_b*a + b_unlinked[new_link_index]] = 1  
+                C[N_b*a + b_unlinked[new_link_index]] = 1
+                for k in range(K):
+                    temp_l_instances[k,int(10*comparison_arrays[k,(N_b*a + b_unlinked[new_link_index])]),0] -= 1
+                    temp_l_instances[k,int(10*comparison_arrays[k,(N_b*a + b_unlinked[new_link_index])]),1] += 1
+
     return(C)
 
 fill_comparison_arrays()
 
-theta_values = theta_and_c_sampler(10)
+print("Comparison Arrays:")
+print(comparison_arrays)
+print("L Instances:")
+print(base_l_instaces)
+
+C = theta_and_c_sampler(100)
+
 C_dataframe = pd.DataFrame(index=range(N_a), columns=range(N_b))
 for a in range(N_a):
     for b in range(N_b):
-        C_dataframe.iat[a, b] = theta_values[N_b*a +b]
+        C_dataframe.iat[a, b] = C[N_b*a + b]
 
 print("C Structure:")
 print(C_dataframe)
+print("--- %s seconds ---" % (time.time() - start_time))
